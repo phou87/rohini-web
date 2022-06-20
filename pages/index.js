31ec8b59f0da8c33b731496c1c3a64dec9b52d0a
Paul Hou <lostinthought87@gmail.com>
2022-06-19 22:20:08 -0400

    smarter title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {Experiment} from '../components/experiment';
+import {useVersion} from '../util/use-version';
 import {BaseProvider, DarkTheme} from 'baseui';
 import {Provider as StyletronProvider} from 'styletron-react';
 import { Client, Server } from "styletron-engine-atomic";
@@ -11,6 +12,8 @@ const engine =
     : new Client();
 
 export default function Home() {
+  const version = useVersion()
+
   return (
     <StyletronProvider value={engine}>
       <BaseProvider overrides={{
@@ -22,7 +25,7 @@ export default function Home() {
       }} theme={DarkTheme}>
         <div className={styles.container}>
           <Head>
-            <title>Emotion Communication Task</title>
+            <title>Emotion Communication Task Visit {version}</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
 
